ea825ac951fa2299017f3f1f5042edf76c721ba2
smlng <s@mlng.net>
2016-02-02 11:40:25 +0100

    add missing file

---
+++ src/sensor_node/poem.h
@@ -0,0 +1,62 @@
+/**
+ * @author <various people on the Internet>
+ * @link <http://altlasten.lutz.donnerhacke.de/mitarb/lutz/usenet/Rainer.html>
+ */
+
+ #ifndef POEM_H_
+ #define POEM_H_ 1
+ 
+char* poem[] = {
+    "Der Erlrouter",
+    "Wer routet so spaet durch Nacht und Wind?",
+    "Es ist der Router, er routet geschwind!",
+    "Bald routet er hier, bald routet er dort",
+    "Jedoch die Pakete, sie kommen nicht fort.",
+    "Sie sammeln und draengeln sich, warten recht lange",
+    "in einer zu niedrig priorisierten Schlange.",
+    "Die Schlangen sind voll, der Router im Stress,",
+    "da meldet sich vorlaut der Routingprozess",
+    "und ruft: All Ihr Paeckchen, Ihr sorgt Euch zu viel,",
+    "nicht der IP-Host, nein, der Weg ist das Ziel!",
+    "Es komme gar bald einem jeden zu Gute",
+    "eine sorgsam geplante und loopfreie Route.",
+    "Des Netzes verschlungene Topologie",
+    "entwirr' ich mit Dijkstras Zeremonie.",
+    "Der Lohn, eine herrliche Routingtabelle,",
+    "dort steh'n sogar Routen zu Himmel und Hoelle.",
+    "Vergiftet der Rueckweg, das Blickfeld gespalten,",
+    "mit RIP wird die Welt nur zum Narren gehalten.",
+    "Doch OSPF durchsucht schnell und bequem",
+    "mein ganz und gar autonomes System.",
+    "Fuer kunstvolle Routen, das vergesst bitte nie,",
+    "benoetigt man Kenntnis der Topologie.",
+    "Zu Ueberraschungs- und Managementzwecken",
+    "durchsuch' ich mit RMON die hintersten Ecken.",
+    "Kein Winkel des Netzes bleibt vor mir verborgen,",
+    "mit SNMP kann ich alles besorgen.",
+    "Wohlan nun, Ihr Paeckchen, die Reise beginnt,",
+    "Mit jeder Station Eure Lebenszeit rinnt.",
+    "Doch halt, Ihr Paeckchen, bevor ich's vergesse:",
+    "Besorgt euch mit NAT eine neue Adresse!",
+    "Mein Router, mein Router, was wird mir so bang!",
+    "Der Weg durch das WAN ist gefaehrlich und lang.",
+    "Mein Paeckchen, mein Paeckchen, so fuerchte Dich nicht,",
+    "denn ueber Dich wacht eine Sicherungsschicht.",
+    "Mein Router, mein Router, was wird mir so flau!",
+    "Dort draussen am LAN-Port, da wartet die MAU!",
+    "Mein Paeckchen, mein Paeckchen Dir droht nicht der Tod,",
+    "denn ueber Dich wacht ja der Manchester-Code.",
+    "Doch halte dich fern von der flammenden Mauer.",
+    "Die sorgt selbst bei mir noch fuer aengstliche Schauer.",
+    "Mein Router, mein Router, wie glaenzt dort voll Tuecke",
+    "der schmale und schluepfrige Weg auf der Bruecke.",
+    "Oh weh! Das Netz ist mit Broadcasts geflutet.",
+    "Ach haett' ich doch niemals zur Bruecke geroutet!",
+    "Mein Paeckchen, den Kopf hoch, Du musst nicht verzagen,",
+    "an Dich wird sich niemals ein Bitfehler wagen.",
+    "Schnell wie der Wind geht die Reise nun weiter",
+    "durch helle und funkelnde Lichtwellenleiter."
+};
+
+size_t poem_len = 49;
+#endif // POEM_H_
