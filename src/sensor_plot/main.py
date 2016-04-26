1cfdd6d9c3c065cb6d1f87c229339d50ca57e293
smlng <s@mlng.net>
2016-04-26 20:02:54 +0200

    sensor_plot: add airquality

--- src/sensor_plot/main.py
+++ src/sensor_plot/main.py
@@ -13,17 +13,20 @@ def main():
     while True:
         req_temperature = Message(code=GET)
         req_humidity = Message(code=GET)
+        req_airquality = Message(code=GET)
         req_temperature.set_request_uri('coap://'+host_uri+'/temperature')
         req_humidity.set_request_uri('coap://'+host_uri+'/humidity')
+        req_airquality.set_request_uri('coap://'+host_uri+'/airquality')
 
         try:
             res_temperature = yield from protocol.request(req_temperature).response
             res_humidity = yield from protocol.request(req_humidity).response
+            res_airquality = yield from protocol.request(req_airquality).response
         except Exception as e:
             print('Failed to fetch resource:')
             print(e)
         else:
-            print('Temperatur: %s, Humitdy: %s' %(res_temperature.payload.decode('utf-8'),res_humidity.payload.decode('utf-8')))
+            print('Temperatur: %s, Humitdy: %s, AirQuality: %s' %(res_temperature.payload.decode('utf-8'), res_humidity.payload.decode('utf-8'), res_airquality.payload.decode('utf-8')))
         time.sleep(1)
 
 if __name__ == "__main__":
