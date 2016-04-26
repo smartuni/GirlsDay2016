a5b8ec60c141c92d1f92d8b69acdaf2304dc3e84
smlng <s@mlng.net>
2016-04-26 19:14:22 +0200

    add sensor_plot

---
+++ src/sensor_plot/main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from aiocoap import *
+import asyncio
+import time
+
+host_uri = "[fe80::f353:4e59:71ba:600a%lowpan0]"
+
+@asyncio.coroutine
+def main():
+    protocol = yield from Context.create_client_context()
+
+    while True:
+        req_temperature = Message(code=GET)
+        req_humidity = Message(code=GET)
+        req_temperature.set_request_uri('coap://'+host_uri+'/temperature')
+        req_humidity.set_request_uri('coap://'+host_uri+'/humidity')
+
+        try:
+            res_temperature = yield from protocol.request(req_temperature).response
+            res_humidity = yield from protocol.request(req_humidity).response
+        except Exception as e:
+            print('Failed to fetch resource:')
+            print(e)
+        else:
+            print('Temperatur: %s, Humitdy: %s' %(res_temperature.payload.decode('utf-8'),res_humidity.payload.decode('utf-8')))
+        time.sleep(1)
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(main())
