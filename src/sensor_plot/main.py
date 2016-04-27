ba237a3f38585337ca3b9f02572919ae2232635b
smlng <s@mlng.net>
2016-04-27 12:05:55 +0200

    sensor_plot: (re)add sleep

--- src/sensor_plot/main.py
+++ src/sensor_plot/main.py
@@ -124,6 +124,7 @@ def main():
                 plt.legend(['SensorA', 'SensorB'], loc="best")
             plt.savefig('sensordata.png')
             plt.close(fig)
+        time.sleep(0.8)
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(main())
